612eaa542271fbc01ad52f1a455e8142a68dba5e
vikions <plambir1994@mail.ru>
2025-06-29 00:01:01 +0700

    fix: use property attribute for frame tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,19 @@ const appUrl = process.env.NEXT_PUBLIC_APP_URL;
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "NEXTDAY: BTC Price Prediction",
-    // Мы перемещаем все fc:frame теги сюда, в объект openGraph
-    // чтобы Next.js сгенерировал их с атрибутом 'property'
     openGraph: {
       title: "NEXTDAY: BTC Price Prediction",
       images: [`${appUrl}/image.png`],
-      other: {
-        "fc:frame": "vNext",
-        "fc:frame:image": `${appUrl}/image.png`,
-        "fc:frame:post_url": `${appUrl}/api/vote`,
-        "fc:frame:button:1": "Higher ⬆️",
-        "fc:frame:button:2": "Lower ⬇️",
-      },
     },
-    // Объект 'other' нам больше не нужен для фреймов
+    other: {
+      // Это правильное место для fc:frame тегов.
+      // Next.js автоматически сгенерирует их с атрибутом 'property'.
+      "fc:frame": "vNext",
+      "fc:frame:image": `${appUrl}/image.png`,
+      "fc:frame:post_url": `${appUrl}/api/vote`,
+      "fc:frame:button:1": "Higher ⬆️",
+      "fc:frame:button:2": "Lower ⬇️",
+    },
   };
 }
 
